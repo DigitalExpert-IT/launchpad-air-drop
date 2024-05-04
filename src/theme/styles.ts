bfd975722b69ba9f60af138bd61c886b8cc3a95d
dedy <dedybramayadi0@gmail.com>
2024-05-04 20:30:46 +0700

    init launchopad airdrop

---
+++ src/theme/styles.ts
@@ -0,0 +1,19 @@
+import { Styles } from "@chakra-ui/theme-tools";
+
+export const styles: Styles = {
+  global: {
+    body: {
+      color: "chakra-body-text",
+      bg: "black",
+      transitionProperty: "background-color",
+      transitionDuration: "normal",
+      lineHeight: "base",
+    },
+    "*::placeholder": {
+      color: "chakra-placeholder-color",
+    },
+    "*, *::before, &::after": {
+      borderColor: "chakra-border-color",
+    },
+  },
+};
