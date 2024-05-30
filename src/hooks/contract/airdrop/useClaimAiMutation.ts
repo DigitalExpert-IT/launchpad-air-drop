bdcc701c5e81c4afe6f774aa7020c57e141b96dc
vickoeff <vic.eff3@gmail.com>
2024-05-30 18:24:33 +0700

    enh: adjust FE with new Contract update

--- src/hooks/contract/airdrop/useClaimAiMutation.ts
+++ src/hooks/contract/airdrop/useClaimAiMutation.ts
@@ -1,13 +1,21 @@
 import { useContractWrite } from "@thirdweb-dev/react";
 import { useAirdropContract } from "./useAirdropContract"
+import { usePair24h } from "@/hooks/useCrypto";
+import { toBn } from "evm-bn";
 
 export const useClaimAiMutation = () => {
   const { contract } = useAirdropContract();
   const { mutateAsync: mutateAsyncClaim, ...rest } = useContractWrite(contract, "claimAi");
 
+  const firstPair = "AI";
+  const secondPair = "USDT";
+  const { lastPrice } = usePair24h(
+    firstPair.concat(secondPair), 8000
+  );
+
   const claim = async (_amount: Number) => {
     try {
-      const { receipt } = await mutateAsyncClaim({ args: [_amount] })
+      const { receipt } = await mutateAsyncClaim({ args: [_amount, toBn(lastPrice.toString(), 18)] })
       return receipt;
     } catch (error) {
       throw error;
--- src/hooks/contract/airdrop/useRegisterMutation.ts
+++ src/hooks/contract/airdrop/useRegisterMutation.ts
@@ -2,8 +2,7 @@ import { useAddress, useContractWrite } from "@thirdweb-dev/react";
 import { useApproveMutation, useBnbBalance, useBnbContract } from "../bnb";
 import { useAirdropContract } from "./useAirdropContract"
 import { toBn } from "evm-bn";
-import { BigNumber } from "ethers";
-import { BNB_DECIMAL } from "@/constants/tokenDecimals";
+import { usePair24h } from "@/hooks/useCrypto";
 
 export const useRegisterMutation = () => {
   const address = useAddress();
@@ -13,18 +12,23 @@ export const useRegisterMutation = () => {
   const { mutateAsync: mutateAsyncApprove } = useApproveMutation();
   const { mutateAsync: mutateAsyncRegister, ...rest } = useContractWrite(contract, "register");
 
-  const register = async (_referral: string, _uri: string, ) => {
-    console.log("referral", _referral);
-    console.log("uri", _uri);
-    
-    console.log("receipt", toBn("5", 18).div(BigInt("1000")));
-    try {
-        const { receipt } = await mutateAsyncRegister({
-          args: [_referral, _uri],
-          overrides: {value: toBn("5", 18).div(BigInt("1000"))}
-        })
+  const firstPair = "AI";
+  const secondPair = "USDT";
+  const { lastPrice } = usePair24h(
+    firstPair.concat(secondPair), 8000
+  );
 
-        console.log("receipt", receipt);
+  const register = async (_referral: string, _uri: string,) => {
+
+    console.log("last price regis", toBn(lastPrice.toString(), 18));
+
+    try {
+      const { receipt } = await mutateAsyncRegister({
+        args: [_referral, _uri, toBn(lastPrice.toString(), 18)],
+        overrides: { value: toBn("5", 18).div(BigInt("1000")) }
+      })
+
+      console.log("receipt", receipt);
 
       return receipt;
     } catch (error) {
