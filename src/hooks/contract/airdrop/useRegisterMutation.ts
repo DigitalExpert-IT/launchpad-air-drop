4ffb3a8a05c609d7843de1c6af0bcce387bea539
vickoeff <vic.eff3@gmail.com>
2024-06-01 13:42:33 +0700

    feat: add success register toast

--- src/hooks/contract/airdrop/useRegisterMutation.ts
+++ src/hooks/contract/airdrop/useRegisterMutation.ts
@@ -2,9 +2,12 @@ import { useContractWrite } from "@thirdweb-dev/react";
 import { useAirdropContract } from "./useAirdropContract"
 import { toBn } from "evm-bn";
 import { usePair24h } from "@/hooks/useCrypto";
+import { useAsyncCall } from "@/hooks/useAsyncCall";
+import { useTranslation } from "react-i18next";
 
 export const useRegisterMutation = () => {
   const { contract } = useAirdropContract();
+  const { t } = useTranslation();
   const { mutateAsync: mutateAsyncRegister, ...rest } = useContractWrite(contract, "register");
 
   const firstPair = "AI";
@@ -13,7 +16,7 @@ export const useRegisterMutation = () => {
     firstPair.concat(secondPair), 8000
   );
 
-  const register = async (_referral: string, _uri: string,) => {
+  const registerContract = async (_referral: string, _uri: string,) => {
     try {
       const { receipt } = await mutateAsyncRegister({
         args: [_referral, _uri, toBn(lastPrice.toString(), 18)],
@@ -29,7 +32,9 @@ export const useRegisterMutation = () => {
     } catch (error) {
       throw error;
     }
-  }
+  };
+
+  const { exec: register } = useAsyncCall(registerContract, t("success.register"));
 
   return { register, ...rest };
 }
