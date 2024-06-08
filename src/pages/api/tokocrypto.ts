4e96f0cb124b0138cc224508735ce80065f2da62
vickoeff <vic.eff3@gmail.com>
2024-06-08 09:37:51 +0700

    update tokocrypto

--- src/pages/api/tokocrypto.ts
+++ src/pages/api/tokocrypto.ts
@@ -15,15 +15,26 @@ type ResponseData = {
   };
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>
+type ResponseError = {
+  error: string;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData | ResponseError>
 ) {
   const { query } = req;
 
   try {
     const tokocrypto = new ccxt.tokocrypto();
+    // tokocrypto.proxyUrl = "http://103.178.42.102:8181/";
     const markets = await tokocrypto.fetchTicker("AI/USDT");
     res.status(200).json(markets);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } catch (e) {
+    if (e instanceof ccxt.NetworkError) {
+      res.status(500).json({ error: `fetchTicker failed due to a network error: ${e.message}` });
+    } else if (e instanceof ccxt.ExchangeError) {
+      res.status(500).json({ error: `fetchTicker failed due to exchange error: ${e.message}` });
+    } else {
+      res.status(500).json({ error: `fetchTicker failed with: ${e.message}` });
+    }
   }
 }
